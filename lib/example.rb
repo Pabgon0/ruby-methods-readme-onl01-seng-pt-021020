d503d3f94e7537ba7609bf9ee6d31baba6141666
Pablo Gonzalez <pabgon0@gmail.com>
2020-02-10 15:19:54 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -1,13 +1,14 @@
 # Use this space to code along with the readme
-def say_hello_world_ten_times
+def greeting
   phrase = "Hello World!"
   puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+end
